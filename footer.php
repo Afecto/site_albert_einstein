797154092579b4d99812b3ca767dc074b9167c25
ArnaldoBR <46871702+ArnaldoBR@users.noreply.github.com>
2019-07-31 10:54:33 -0300

    Footer.

---
+++ footer.php
@@ -0,0 +1,44 @@
+		<footer class="container sticky-bottom border-top">
+			<section class="row">
+				<section class="col list-unstyled col-md">
+					<img id="cps" src="img/cps.png" height="80px"/>
+				</section>
+				<section class="col list-unstyled col-md">
+					<img id="brasaoSP" src="img/brasaoSP.PNG" height="80px"/>
+				</section>
+				<section class="col list-unstyled col-md">
+					<li><a class="text-muted" href="#">CONHEÇA O EINSTEIN</a></li>
+					<li><a class="text-muted" href="#">CORPO DOCENTE</a></li>
+					<li><a class="text-muted" href="#">DIRETORIA</a></li>
+					<li><a class="text-muted" href="#">PROFESSORES</a></li>
+					<li><a class="text-muted" href="#">DIRETORIA DE SERVIÇOS</a></li>
+					<li><a class="text-muted" href="#">NOSSA HISTÓRIA</a></li>
+					<li><a class="text-muted" href="#">CONSELHO DE ESCOLA</a></li>
+					<li><a class="text-muted" href="#">ATA DO CONSELHO DE ESCOLA</a></li>
+					<li><a class="text-muted" href="#">APM</a></li>
+				</section>
+				<section class="col list-unstyled col-md">
+					<li><a class="text-muted" href="#">CURSOS</a></li>
+					<li><a class="text-muted" href="#">EXTENÇÕES</a></li>
+					<li><a class="text-muted" href="#">ETIM E MÉDIO</a></li>
+					<li><a class="text-muted" href="#">TÉCNICO</a></li>
+				</section>
+				<section class="col list-unstyled col-md">
+					<li><a class="text-muted" href="#">PROJETOS</a></li>
+				</section>
+				<section class="col list-unstyled col-md">
+					<li><a class="text-muted" href="#">ÁREA DO ALUNO</a></li>
+					<li><a class="text-muted" href="#">GRÊMIO</a></li>
+					<li><a class="text-muted" href="#">PPP E PPG</a></li>
+				</section>
+				<section class="col list-unstyled col-md">
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3658.760792925923!2d-46.66214668454297!3d-23.505123984710828!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cef7ee0de36baf%3A0x9659f432c6b1ecc!2sR.+Nova+Granada%2C+35+-+Vila+Baruel%2C+S%C3%A3o+Paulo+-+SP%2C+02522-050!5e0!3m2!1spt-BR!2sbr!4v1559264025829!5m2!1spt-BR!2sbr" height="150px" width="150px" frameborder="0" style="border:0" allowfullscreen></iframe>
+				</section>
+			</section>
+			
+			<section id="copyrigth" class="text-muted text-center">
+				<section class="mb-1">
+					<p>&copy; Copyright 2019 - ETEC Albert Einstein</p>
+				</section>
+			</section>
+		</footer>
