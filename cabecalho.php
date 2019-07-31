b5f5210defa7decd7b9d213bd89c42862fbde334
ArnaldoBR <46871702+ArnaldoBR@users.noreply.github.com>
2019-07-31 10:53:22 -0300

    Header.

---
+++ cabecalho.php
@@ -0,0 +1,30 @@
+	<!-- <div id="kp-page-header" class="header-style-1">
+            </div> -->
+		<!-- Cabeçalho-Einstein -->
+		<header class="container">
+			<!-- img.Einstein -->
+		<div id="kp-page-header" class="header-style-1">
+			<section class="pt-2 pb-2" id="header-top">
+                <!-- top-menu -->
+                    <ul type="none" id="contact-top-box" class="pull-right clearfix"> 
+                        <li><span class="clearfix">
+							<span class="fa fa-map-marker"></span>R. Nova Granada, 35 - Casa Verde - Vila Baruel, São Paulo - SP 02522-050</span>
+						</li>
+                        <li><span class="clearfix">
+							<span class="fa fa-envelope"></span>aeinst@uol.com.br &nbsp &nbsp &nbsp | &nbsp &nbsp &nbsp Tel.: +55(11) 3858-7529 ou +55(11) 3966-0503</span>
+						</li>
+                    </ul>
+                    <!-- contact-top-box -->
+                    <div class="clear"></div>
+			</section>
+			<section class="cabecalho pb-4 pt-4">
+			<figure>
+				<img id="cps" class="cps" src="img/cps.png" height="160px"/>
+				<img id="albertEinstein" class="albertEinstein" src="img/albertEinstein.png" height="160px"/>
+				<img id="brasaoSP" class="brasaoSP" src="img/brasaoSP.PNG" height="160px"/>
+				</figure>	
+			</section>
+			<!-- fim.img.Einstein -->
+		</div>
+		</header>
+		<!-- Fim.Cabeçalho-Einstein -->
